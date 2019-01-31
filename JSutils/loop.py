bc34944352011470396b21b248582625831c9d3f
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-01-31 13:22:48 +0800

    Changes: 	Finally fixed the loop module to make it working. Created a workaround 	with the flag setting problem and program design. Currently works and 	have methods to stop the loop and restart and more with the use of 	Processes and its ability to terminate child processes.

--- JSutils/loop.py
+++ JSutils/loop.py
@@ -40,71 +40,83 @@ class setInterval:
         This is used to create a type of loop, where a given function/method is called repeatedly every
         'n' given seconds, until the stop method is called, or when the main thread dies/exits. Used this
         to emulate the behaviour of the native setInterval function in JavaScript.
+
+        When we say run this every 10 Seconds. Do we mean to execute the program every 10 seconds or do we mean to
+        have 10 seconds time interval between the end of function execution 1 and the start of function execution 2?
+
+        How does my other loop deal with this?
         """
 
     # To pass in the interval time, callback function, and any arguements for the callback function into the constructor
     def __init__(self, time, fn, *args, **kwargs):
         self.__time = time
-        self.fn = fn
-        self.args = args
-        self.kwargs = kwargs
-        # Instead of calling input function on object creation, wait till end of the 1st time interval.
-        # Delay the first call to the timeout method by the given time interval
-        # self.__thread = Thread(target=self.start) # Testing to use threads, so I can make it a daemon to die on main thread exits.
+        self.__fn = fn
+        self.__args = args
+        self.__kwargs = kwargs
+        # Start looping
+        self.start()
 
-        # self.task = Process(target=self.start)
-        self.task = Thread(target=self.start)
-
-        self.task.daemon = True  # Daemonize thread
-        self.task.start()  # Start the execution
+    # Wrapper method for the loop. Should not be called externally or by any other method except the start method
+    def _loop(self):
+        # Loop that runs forever until this process is killed
+        while True:
+            # Sleep first before calling the function.
+            sleep(self.__time)
+            self.__fn(*self.__args, **self.__kwargs)
 
     def start(self):
-        """ Method that runs forever until main process/thread is killed """
-        
-        from threading import Timer
-        Timer.start()
-
-        while flag.isSet:
-            # sleep(self.__time)
-            # self.fn(*self.args, **self.kwargs)
-
-            sleep(self.__time)
-            self.fn(*self.args, **self.kwargs)
+        """ Method that calls the infinite loop's wrapper function """
+        # Create a new child process to run the infinite loop to make it non blocking.
+        self.__loop_process = Process(target=self._loop)
+        self.__loop_process.daemon = True # Daemonize the process, so when the main process dies, it is killed too.
+        self.__loop_process.start() # Start the execution within the child process
 
     # Method to stop the loop, if needed, execute the callback one last time
     def stop(self, oneLastTime=False):
-        # Kill the task first thing first
-        # self.task.terminate() # Terminate is for Processes only
-        # self.task.
+        # Kill the loop process first thing first
+        self.__loop_process.terminate()  # Terminate is for Processes only
 
         # If 'oneLastTime' is True, Call the given function with any arguements supplied for the last time.
         if oneLastTime:
-            self.fn(*self.args, **self.kwargs)
+            self.__fn(*self.__args, **self.__kwargs)
+
+    def restart(self):
+        # Stop/Kill the current loop process
+        self.stop()
+        # Create a new Process to run the loop
+        self.start()
 
     # Set/Change the interval for which the timer takes to timeout. New interval will start immediately.
     def set_interval(self, time):
-        # Stop the current timer
-        self.stop()
+        """ May be slow as the old process is killed and a new Process needs to be respawned """
         # Set the time into the object's field
         self.__time = time
-        # Reset the timer with a newly created Timer object using start method.
-        self.start()
+        # Start new loop with new attributes
+        self.restart()
 
     # Set the arguements to be passed into the callback function
     def set_args(self, *args, **kwargs):
         # Set the input arguements into the object's fields.
-        self.args = args
-        self.kwargs = kwargs
+        self.__args = args
+        self.__kwargs = kwargs
+        # Start new loop with new attributes
+        self.restart()
 
 
-def hello(msg):
-    print('Chicken ', msg)
+def hello(msg, invald_input=None, optionalMsg=None):
+    print('Chicken ', msg, ' ', optionalMsg)
+
 
 if __name__ == "__main__":
     # If this module called as a standalone module to see how it works, then run the below example code
-    setInterval(1, hello, 'nuggets')
+    intervaloop = setInterval(1, hello, 'nuggets')
 
-    while True:
-        pass
+    sleep(3)
 
+    intervaloop.set_interval(0.1)
+    # intervaloop.set_args('hello', optionalMsg='HELAoscbnknjkjn')
+
+    sleep(3)
+
+    intervaloop.stop()
     # This is a loop and sleep based setInterval commpared to a Timer Object based setInterval
